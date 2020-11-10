e0dc70fe41266a815806fbbce8e1cd711498bb26
vocoWone <605131105@qq.com>
2020-11-10 14:38:04 +0800

    update config

--- config/base.js
+++ config/base.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const fs = require("fs-extra");
+const customHtmlTemplatePath = path.join(process.cwd(),"src/index.html")
 
 const config = {
     port:8080,
     entry:path.join(process.cwd(),"src/index.tsx"),
     contentBase: path.join(process.cwd(),"src/static"),
-    htmlTemplate:path.join(process.cwd(),"src/index.html") || path.resolve(__dirname,"../src/index.html")
+    htmlTemplate:fs.pathExistsSync(customHtmlTemplatePath) ? customHtmlTemplatePath : path.resolve(__dirname,"../src/index.html")
 }
 
 module.exports = config;
